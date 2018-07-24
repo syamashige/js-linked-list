d548534fe2a90370aabaf810f7f2d372be8acbd0
Sarah <yamashige.sarah@gmail.com>
2018-07-23 19:04:10 -1000

    Starting from the beginning for practice

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,12 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
+    
+
+
+
+
+
 
 }
